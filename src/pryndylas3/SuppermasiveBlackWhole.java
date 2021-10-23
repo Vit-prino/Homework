08b2970baacd702a6a81339f712526bff95123a3
Vit-prino <vitalii.pryndylas@privatbank.ua>
2021-10-23 18:18:50 +0300

    init commit

--- src/pryndylas3/SuppermasiveBlackWhole.java
+++ src/pryndylas3/SuppermasiveBlackWhole.java
@@ -1,5 +1,8 @@
 package pryndylas3;
+//якшо викладач це читає, то я чуть не вмер, напиши що радий що я вижив в коменті до завдання)
+
 import java.util.Scanner;
+
 public class SuppermasiveBlackWhole {
     public static void main(String[] args) {
 
@@ -8,32 +11,41 @@ public class SuppermasiveBlackWhole {
         int mas[] = new int[10];
         for (int i = 0; i < mas.length; i++) {
             mas[i] = input.nextInt();
-        }System.out.print("Ви ввели");
-        for (int i=0;i < mas.length;i++){
+        }
+        System.out.print("Ви ввели");
+        for (int i = 0; i < mas.length; i++) {
 
-            {
-                System.out.print(mas[i] + " ");
-            }}
+
+            System.out.print(mas[i] + " ");
+        }
         int n = mas.length;
-        for (int i= 0;i <= n - 1; i++){
-            for (int j = 0;j <n - 1; j++)
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j < n - 1; j++)
                 if (mas[j] > mas[j + 1]) {
-                   int temp = mas[j];
+                    int temp = mas[j];
                     mas[j] = mas[j + 1];
                     mas[j + 1] = temp;
                 }
         }
-        for (int i = 0; i<n; i++){
-            System.out.print(+mas[i]);
-            }
-        System.out.println();
+        System.out.println("Відсортовано з помощю богів і бабла");
+        for (int i = 0; i < n; i++) {
+            System.out.print(+mas[i] + ".");
+        }
+        //System.out.println();
         int num = 0;
-        for (int i=0 ; i< mas.length; i++){
-            if (mas[i]>0 ){
+        int sum = 0;
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i] > 0) {
                 num++;
+                sum = mas[i]+sum;
+            }
+            else {
+                sum = mas[i]+sum;
+
             }
         }
-        System.out.println("Кількість елементів більше 0  =  "+ num);
+        System.out.println("Кількість елементів більше 0  =  " + num);
+        System.out.println("Сумма елементів  =  " + sum);
 
 
     }
