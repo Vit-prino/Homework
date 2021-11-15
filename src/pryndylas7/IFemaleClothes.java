23d0c0c83fb3c7234765571215fc3924c9408d8d
Vit-prino <vitalii.pryndylas@privatbank.ua>
2021-11-15 22:11:10 +0200

    init commit

---
+++ src/pryndylas7/IFemaleClothes.java
@@ -0,0 +1,7 @@
+package pryndylas7;
+
+public class IFemaleClothes {
+    default void dressFemale() {
+        System.out.println("Одеваем женщину");
+    }
+}
---
+++ src/pryndylas7/IMaleClothes.java
@@ -0,0 +1,8 @@
+package pryndylas7;
+
+public class IMaleClothes {
+    public interface IMaleClothes {
+        default void dressMale() {
+            System.out.println("Одеваем мужчину");
+        }
+}
