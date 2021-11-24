e78f1dd199daf1d2a2f8b189da46f29d404bd437
Vit-prino <vitalii.pryndylas@privatbank.ua>
2021-11-24 14:27:13 +0200

    init commit

--- src/pryndylas8/OnlineShop.java
+++ src/pryndylas8/OnlineShop.java
@@ -2,33 +2,48 @@ package pryndylas8;
 
 import java.util.Scanner;
 public class OnlineShop {
-
-    public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
+    public static void main(String[] args) throws Auth.WrongLoginException, Auth.WrongPasswordException {
         Auth authUp = new Auth();
         Scanner init = new Scanner(System.in);
-        System.out.println("Вітаю на нашому магазині. Для початку Закупів з скидкою в 50% вам необхідно зареєструватися.");
-        System.out.print("Введіть ваш Логін >>");
-        String rLogin = init.nextLine();
-        System.out.print("Введіть ваш Пароль >>");
-        String rPass = init.nextLine();
-        System.out.print("Повторіть пароль >>");
-        String rPass2 = init.nextLine();
-        authUp.signUp(rLogin,rPass,rPass2);
+        int r;
 
+        System.out.println("Вітаю на нашому магазині. Для початку Закупів з раббaтой в 50% вам необхідно зареєструватися.");
+        do {
+            r = 0;
+            try {
+                System.out.print("Введіть ваш Логін >>");
+                String rLogin = init.nextLine();
+                System.out.print("Введіть ваш Пароль >>");
+                String rPass = init.nextLine();
+                System.out.print("Повторіть пароль >>");
+                String rPass2 = init.nextLine();
+                authUp.signUp(rLogin, rPass, rPass2);
+            } catch (Auth.WrongLoginException | Auth.WrongPasswordException e) {
+                System.out.println(e);
+                r = 1;
+            }
+        } while (r == 1);
 
         System.out.println("*************************");
-        System.out.println ("* Вход пользователя *");
+        System.out.println("* Вход пользователя *");
         System.out.println("*************************");
+        do {
+            r = 0;
+            try {
+                System.out.println("Введіть ваш Логін >>");
+                String authLogin = init.nextLine();
+                System.out.println("Введіть ваш Пароль >>");
+                String authPass = init.nextLine();
 
-        System.out.println("Введіть ваш Логін >>");
-        String authLogin = init.nextLine();
-        System.out.println("Введіть ваш Пароль >>");
-        String authPass = init.nextLine();
-        authUp.signIn(authLogin,authPass);
+                authUp.signIn(authLogin, authPass);
+            } catch (Auth.WrongLoginException | Auth.WrongPasswordException e) {
+                System.out.println(e);
+                System.out.println();
+                r = 1;
+            }
+        } while (r == 1);
 
-           // if (login.equals(br.readLine()) && pass.equals(br.readLine()))
-              //  System.out.println("«Успешно вошел в систему, добро пожаловать " + rLogin + "!»");
-         //   System.out.Println(«Имя пользователя и пароль не совпадают, пожалуйста, введите !»);
+
 
 
 
