5120971f83faaef84ac27c6b3c3b367d852bcff5
Vit-prino <vitalii.pryndylas@privatbank.ua>
2021-12-19 13:14:31 +0200

    init commit

---
+++ src/pryndylas11/main.java
@@ -0,0 +1,12 @@
+package pryndylas11;
+public  class main {
+    public static void main(String[] args) throws Exception {
+
+
+        PhoneBook pb = new PhoneBook();
+        pb.printGUI();  // меню
+        pb.run();  //  меню с вибором
+
+    }
+
+}
